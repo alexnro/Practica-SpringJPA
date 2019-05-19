4d32cdbaa65ee1e18e60fa3be5bf9e48c75a3d73
alexnro <alexjandron99@gmail.com>
2019-05-19 13:00:53 +0200

    Doceavo caso test

--- src/main/java/org/formacio/setmana2/servei/ServeiEscola.java
+++ src/main/java/org/formacio/setmana2/servei/ServeiEscola.java
@@ -1,21 +1,30 @@
 package org.formacio.setmana2.servei;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.formacio.setmana2.domini.Matricula;
 import org.formacio.setmana2.repositori.EdatIncorrecteException;
+import org.formacio.setmana2.repositori.RepositoriEscola;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class ServeiEscola {
 
-	
+	@Autowired
+	RepositoriEscola repositoriEscola = null;
+
 	/**
 	 * Important: els alumnes i els cursos indicats JA existeixen a la base de dades.
 	 * Per tant, els hem de carregar, no crear de nou.
 	 * L'excepcio EdatIncorrecteException no s'ha de capturar. S'ha de propagar cap el client
 	 */
 	public List<Matricula> apunta (String curs, List<String> alumnes) throws EdatIncorrecteException {
-		return null;
+		List<Matricula> matriculas = new ArrayList<>();
+		for (String alumne : alumnes) {
+			matriculas.add(repositoriEscola.apunta(alumne, curs));
+		}
+		return matriculas;
     }
 }
